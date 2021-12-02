cd97cd1a12f9aae3aa356e00023aa9ecf759abc5
juststrive007 <juststrive007@gmail.com>
2021-12-02 18:17:14 +0800

    （add） 测试ztzip压缩工具

---
+++ snakeyaml/src/main/java/com/example/snakeyaml/utils/ZtZIpTest.java
@@ -0,0 +1,10 @@
+package com.example.snakeyaml.utils;
+
+import org.zeroturnaround.zip.ZipUtil;
+
+public class ZtZIpTest {
+    public static void main(String[] args) {
+        ZipUtil.unpack();
+        ZipUtil.pack();
+    }
+}
